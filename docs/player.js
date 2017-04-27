1262de47f1803d86e2bacc6037bc723e18b798b4
s16210mk <mkfhyssm.9109@gmail.com>
2017-04-27 12:41:05 +0900

    表か裏かマンに勝つコミットを追加

--- docs/player.js
+++ docs/player.js
@@ -1,9 +1,18 @@
 function action(oppornent){
-  return 1;
-  const GU = 0;
-  const GHOKI = 1;
-  const PA = 2;
+  return 0;
+}
+const GU = 0;
+const CHOKI = 1;
+const PA = 2;
 
-  function action(){
-    return GU;
+function action(){
+  return GU;
+}
+let matches = 2;
+function action(oppornent){
+  matches = matches + 2;
+  const remainder = matches % 2;
+  const isEven = remainder == 2;
+  console.log(`${matches} % 2 = ${remainder} : ${isEven}`);
+  return PA;
 }
