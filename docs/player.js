e56519b9994780f4d929d8353eb6aae1f30b9447
s16210mk <mkfhyssm.9109@gmail.com>
2017-05-18 12:34:37 +0900

    第5回

--- docs/player.js
+++ docs/player.js
@@ -1,18 +1,62 @@
-function action(oppornent){
-  return 0;
-}
 const GU = 0;
 const CHOKI = 1;
 const PA = 2;
 
-function action(){
+let matches = 0
+
+
+function countup() {
+  matches = matches + 1
+  return matches;
+}
+
+//チョキ大好きマンに対するアクション
+function actionAgainstChokiDaisukiman() {
   return GU;
 }
-let matches = 2;
+
+// 表か裏かマンに対するアクション
+function actionAgainstOmoteUra(){
+  const isEven = matches % 2 == 0;
+  let result = PA;
+  if (isEven){
+    result = GU;
+  }
+  return result;
+}
+
+// 帰ってきた表か裏かマンに対するアクション
+function actionAgainstOmoteUra2(){
+  const isEven = matches % 3 == 0;
+  let result = GU;
+  if (isEven){
+    result = CHOKI;
+  }
+  return result;
+}
+
+/*
+対戦相手名前まとめ
+
+チョキ大好きマン： fighter::choki-lover
+表か裏かマン: fighter::odd-even
+帰ってきた表か裏かマン： fighter::on-third
+順番に出していくマン： fighter::rotation
+*/
+
+
 function action(oppornent){
-  matches = matches + 2;
-  const remainder = matches % 2;
-  const isEven = remainder == 2;
-  console.log(`${matches} % 2 = ${remainder} : ${isEven}`);
-  return PA;
+  console.log(opprnent)
+  let resurt = GU;
+  countup();
+  if (oppornent == "fighter::choki-lover"){
+    result = actionAgainstChokiDaisukiman();
+  } else if (oppornent == "fighter::odd-even"){
+      resurt = actionAgainstOmoteUra();
+  } else if(oppornent == "fighter::rotation") {
+      resurt = actionAgainstrotation()
+  } else {
+      result = actionAgainstOmoteUra2();
+  }
+  return result;
 }
